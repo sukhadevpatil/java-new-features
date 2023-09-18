aee771c3670146f5e16be14b41aab7d079957780
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-09-18 23:30:12 +0530

    nth index element

--- src/main/java/com/core/concepts/newfeatures/basics/ConcurrentExceptionFix.java
+++ src/main/java/com/core/concepts/newfeatures/basics/ConcurrentExceptionFix.java
@@ -1,2 +1,74 @@
-package com.core.concepts.newfeatures.basics;public class ConcurrentExceptionFix {
+package com.core.concepts.newfeatures.basics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConcurrentExceptionFix {
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(4);
+        integers.add(2);
+        integers.add(3);
+
+        //This will throw exception
+        /*for (Integer integer : integers) {
+            integers.remove(1);
+        }*/
+
+        List<Integer> toRemove = new ArrayList<>();
+
+        for (Integer integer : integers) {
+            if(integer == 1) {
+                toRemove.add(integer);
+            }
+        }
+        integers.removeAll(toRemove);
+
+        System.out.println(integers);
+
+        System.out.println("==================================================");
+
+        for(Iterator<Integer> iterator = integers.iterator(); iterator.hasNext();) {
+            Integer integer = iterator.next();
+            if(integer == 2) {
+                iterator.remove();
+            }
+        }
+
+        //integers.removeIf(integer -> integer == 2);
+
+        System.out.println(integers);
+
+        System.out.println("============================================");
+        integers.add(1);
+        integers.add(2);
+
+        List<Integer> list = integers.stream().filter(val -> val != 4).toList();
+
+        System.out.println(list);
+
+        System.out.println("===============================================");
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+
+        // Iterating over object elements using iterator
+        Iterator<Integer> iterator = list1.iterator();
+
+        while (iterator.hasNext()) {
+            Integer value = iterator.next();
+
+            // If element equals certain value
+            if (value.equals(2)) {
+                iterator.remove();
+            }
+        }
+    }
 }
--- src/main/java/com/core/concepts/newfeatures/java8/NthHighestSalary.java
+++ src/main/java/com/core/concepts/newfeatures/java8/NthHighestSalary.java
@@ -1,2 +1,57 @@
-package com.core.concepts.newfeatures.java8;public class NthHighestSalary {
+package com.core.concepts.newfeatures.java8;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NthHighestSalary {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> empMap = new HashMap<>();
+        empMap.put("Dev 1", 4500000);
+        empMap.put("Dev 2", 4100000);
+        empMap.put("Dev 3", 4400000);
+        empMap.put("Dev 4", 4200000);
+
+        Map.Entry<String, Integer> result = getNthHighestSalaryEmp(1, empMap);
+
+        System.out.println(result);
+
+        System.out.println("=================================================");
+
+        empMap.clear();
+        //If the multiple employees have same salaries
+        empMap.put("Dev 1", 4400000);
+        empMap.put("Dev 2", 4400000);
+        empMap.put("Dev 3", 4200000);
+        empMap.put("Dev 4", 4200000);
+        empMap.put("Dev 5", 4500000);
+        empMap.put("Dev 6", 4500000);
+
+        Map.Entry<Integer, List<String>> result2 = getNthHighestSalaryEmpHavingSameSalaries(3, empMap);
+
+        System.out.println(result2);
+
+    }
+
+    private static Map.Entry<Integer, List<String>> getNthHighestSalaryEmpHavingSameSalaries(int indexPosition, Map<String, Integer> empMap) {
+        return empMap.entrySet().stream()
+                .collect(Collectors.groupingBy(Map.Entry :: getValue,
+                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())))
+                .entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByKey()))
+                .collect(Collectors.toList())
+                .get(indexPosition - 1);
+    }
+
+    private static Map.Entry<String, Integer> getNthHighestSalaryEmp(int indexPosition, Map<String, Integer> empMap) {
+
+        return empMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toList())
+                .get(indexPosition - 1);
+    }
 }

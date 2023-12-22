7d88957a85965c7a24bdc0ecc721cf355197c643
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-12-22 20:13:25 +0530

    json to object vice versa example with GSON

--- src/main/java/com/core/concepts/newfeatures/json/gson/GsonExample.java
+++ src/main/java/com/core/concepts/newfeatures/json/gson/GsonExample.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonElement;
 
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Reader;
 import java.math.BigDecimal;
 import java.util.Arrays;

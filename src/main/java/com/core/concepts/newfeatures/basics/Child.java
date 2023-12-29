9547c39f83244aa30b4bcade8e94c9486fee23e5
Sukhdev Patil <sukhadev.patil@gmail.com>
2023-12-29 10:59:28 +0530

    basics

---
+++ src/main/java/com/core/concepts/newfeatures/basics/Child.java
@@ -0,0 +1,21 @@
+package com.core.concepts.newfeatures.basics;
+
+public class Child extends Parent {
+
+    @Override
+    public void hello() {
+        System.out.println("Child hello()...");
+    }
+
+    public static void main(String[] args) {
+        Parent parent = new Child();
+        parent.hello();
+    }
+}
+
+
+class Parent {
+    public void hello() {
+        System.out.println("Parent hello()...");
+    }
+}

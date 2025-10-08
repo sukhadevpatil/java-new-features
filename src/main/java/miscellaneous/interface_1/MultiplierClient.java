7c133244cdf59c52b5a42c4642732b72f0af9571
Sukhdev Patil <sukhadevpatil@gmail.com>
2025-10-08 09:31:00 +0530

    unused line commented

--- src/main/java/miscellaneous/interface_1/MultiplierClient.java
+++ src/main/java/miscellaneous/interface_1/MultiplierClient.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class MultiplierClient {
     public static void main(String[] args) {
-        Multiplier multiplier = new MultiplierImpl();
+        //Multiplier multiplier = new MultiplierImpl();
         List<Integer> list = List.of(1,2,3,4,5);
         System.out.println(Multiplier.isEmpty(list));
     }
